e75d4ed7167cab59a19c19906ec741d463a26bc3
Vil <cardboardburger@gmail.com>
2022-01-08 21:20:23 +0100

    Add broken trait code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
 use std::time::SystemTime;
 
-mod window;
+use minifb::Key;
+
+mod painter;
 mod state;
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 640;
 
-fn update_state(state: &mut state::State) {
+impl painter::Paintable for painter::Painter {
+    fn update_state(state: &mut state::State) {
 
-    let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u32 / 100;
-    state.increment = time;
-
-    let mut count: u32 = 0;
-    for i in state.buffer.iter_mut() {
-        let current_x = count % WIDTH as u32;
-        let current_y = count / HEIGHT as u32;
-        *i = ((current_x + state.increment) & 0xFF) << 8 | (current_y & 0xFF);
-        count += 1;
+        let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u32 / 100;
+        state.increment = time;
+    
+        let mut count: u32 = 0;
+        for i in state.buffer.iter_mut() {
+            let current_x = count % WIDTH as u32;
+            let current_y = count / HEIGHT as u32;
+            *i = ((current_x + state.increment) & 0xFF) << 8 | (current_y & 0xFF);
+            count += 1;
+        }
+    }
+    
+    fn handle_key_presses(key: Vec<Key>, state: &mut state::State) {
+        
     }
 }
 
 fn main() {
     let buffer: Vec<u32> = Vec::with_capacity(WIDTH * HEIGHT);
-    let mut state = state::State { buffer: buffer, increment: 0 };
+    let mut state = state::State { buffer, increment: 0 };
 
-    window::draw_window(WIDTH, HEIGHT, &update_state, &mut state);
+    let painter = painter::Painter {state, width: WIDTH, height: HEIGHT};
+
+    painter.draw_window();
 }
 
 
---
+++ src/painter.rs
@@ -0,0 +1,49 @@
+use minifb::{ScaleMode, Window, WindowOptions, Key};
+
+use crate::{state};
+
+pub struct Painter {
+    pub state: state::State,
+    pub width: usize,
+    pub height: usize
+}
+
+pub trait Paintable {
+    fn update_state(state: &mut state::State);
+    fn handle_key_presses(key: Vec<Key>, state: &mut state::State);
+}
+
+impl Painter{
+    pub fn draw_window(&mut self) {
+
+        let mut window = Window::new(
+            "Press ESC to exit",
+            self.width,
+            self.height,
+            WindowOptions {
+                resize: true,
+                scale_mode: ScaleMode::UpperLeft,
+                ..WindowOptions::default()
+            },
+        )
+        .expect("Unable to create window");
+
+        // Limit to max ~60 fps update rate
+        window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+        
+        let mut size = (0, 0);
+
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+            let new_size = (window.get_size().0, window.get_size().1);
+            if new_size != size {
+                size = new_size;
+                self.state.buffer.resize(size.0 * size.1, 0);
+            }
+            self.update_state(self.state);
+
+            window
+                .update_with_buffer(&self.state.buffer, new_size.0, new_size.1)
+                .unwrap();
+        }
+    }
+}
